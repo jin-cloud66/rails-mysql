36ae7f6a722c24a5f97f97711bd5562009ce3cd0
DimitriosLisenko <DimitriosLisenko@users.noreply.github.com>
2021-01-21 12:12:34 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 	resources :articles
 	
 	root 'welcome#index'
+	
+	get 'health', action: :health, controller: 'health'
+	get 'healthz', action: :healthz, controller: 'health'
 end
