3465bdb9255efc68f809b34f8bc93ec7bdf4346b
jin-cloud66 <82423308+jin-cloud66@users.noreply.github.com>
2021-08-06 07:38:03 -0700

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  connects_to database: { writing: :animals, reading: :animals }
-  
   self.abstract_class = true
+  
+  connects_to database: { writing: :animals, reading: :animals }
 end
