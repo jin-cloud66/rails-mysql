882497858d5c6914188420a75e7688cd1ca727b3
Jing Qin <jing@cloud66.com>
2021-08-07 00:43:44 -0700

    update application_record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   
-  connects_to database: { writing: :animals, reading: :animals_replica }
+  connects_to database: { writing: :animals }
 end
