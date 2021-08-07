8523334edd5b3cebe180871066de0c0dc31a66f6
jin-cloud66 <82423308+jin-cloud66@users.noreply.github.com>
2021-08-06 23:47:45 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Example
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    config.active_record.writing_role = :writes
+    config.active_record.reading_role = :readonly
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
